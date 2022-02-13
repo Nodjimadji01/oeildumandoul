be19d0d0b9a077656328bfba005320f016c8fb19
Nodjimadji <58188444+Nodjimadji@users.noreply.github.com>
2022-02-13 20:40:20 +0100

    add nav link

--- oeildumandoul/resources/js/components/Navigation.js
+++ oeildumandoul/resources/js/components/Navigation.js
@@ -1,69 +1,35 @@
-/*import React from "react";
-
-const Navigation = () => {
+ import React from "react";
+ import "../pages/app.css";
+ const Navigation = () => {
     return (
-        <di className= "nav">
-           <nav>
-               A LA UNE
-           </nav>
-           <nav>
-               A LA UNE
-           </nav>
+         <di className= "nav">
+            <nav className="barre">
+                A LA UNE
+            </nav>
 
-        </di>
+           <nav className="barre">
+                POLITIQUE
+            </nav>
 
-    );
-}
+			<nav className="barre">
+                EDUCATION
+            </nav>
 
-export default Navigation;*/
-import React from 'react';
-import {
-Nav,
-NavLink,
-Bars,
-NavMenu,
-NavBtn,
-NavBtnLink,
-} from './NavbarElements';
+			<nav className="barre">
+                SOCIETE
+            </nav>
 
-const Navigation = () => {
-return (
-	
-	<>
-	<di className= "nav">
-	<Nav>
-		<Bars />
+			<nav className="barre">
+                SPORT
+            </nav>
+			<nav className="barre">
+                FINANCE
+            </nav>
 
-		<NavMenu>
-		<NavLink to='/about' activeStyle>
-			About
-		</NavLink>
-		<NavLink to='/events' activeStyle>
-			Events
-		</NavLink>
-		<NavLink to='/annual' activeStyle>
-			Annual Report
-		</NavLink>
-		<NavLink to='/team' activeStyle>
-			Teams
-		</NavLink>
-		<NavLink to='/blogs' activeStyle>
-			Blogs
-		</NavLink>
-		<NavLink to='/sign-up' activeStyle>
-			Sign Up
-		</NavLink>
-		{/* Second Nav */}
-		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
-		</NavMenu>
-		<NavBtn>
-		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
-		</NavBtn>
-	</Nav>
 
-	</di>
-	</>
-);
-};
+         </di>
 
-export default Navigation;
+   );
+ }
+
+ export default Navigation;
--- oeildumandoul/resources/js/pages/home.js
+++ oeildumandoul/resources/js/pages/home.js
@@ -10,6 +10,19 @@ function Home() {
     return (
         <div className='home' >
             <Header></Header>
+        
+        <div className='carroussel'>
+            <form>
+            <input type="radio" name="fancy" autofocus value="clubs" id="clubs" />
+            <input type="radio" name="fancy" value="hearts" id="hearts" />
+            <input type="radio" name="fancy" value="spades" id="spades" />
+            <input type="radio" name="fancy" value="diamonds" id="diamonds" />			
+            <label for="clubs">&#9827; Clubs</label><label for="hearts">&#9829; Hearts</label><label for="spades">&#9824; Spades</label><label for="diamonds">&#9830; Diamonds</label>
+
+            <div class="keys">Use left and right keys to navigate</div>
+            </form>
+        </div>
+
             <div className="body">
                     <p className="p">
                 L'objectif d'un rapport de la faisabilité consiste à présenter les paramètres du projet et à définir les solutions possibles au problème, au besoin ou à l'occasion précisés. Après avoir lancé des idées sur les solutions possibles, l'équipe de projet expose en détail chacune de ces solutions et fournit suffisamment de renseignements, dont des renseignements très généraux sur les coûts, pour permettre au chef de projet de recommander à l'autorité approbatrice toutes les solutions possibles viables qui devraient être analysées plus en profondeur au cours de la prochaine phase (analyse de rentabilisation). Les contraintes liées au projet et les limites de dépenses font partie des divers facteurs qui permettent de déterminer la viabilité des solutions.
