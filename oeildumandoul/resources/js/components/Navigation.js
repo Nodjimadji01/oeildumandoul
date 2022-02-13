ffa09dc0c109f8f958a6105721d586605a089594
Nodjimadji <58188444+Nodjimadji@users.noreply.github.com>
2022-02-13 20:32:39 +0100

    add nave link abd carroussel

--- oeildumandoul/resources/js/components/Navigation.js
+++ oeildumandoul/resources/js/components/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+/*import React from "react";
 
 const Navigation = () => {
     return (
@@ -14,5 +14,56 @@ const Navigation = () => {
 
     );
 }
+
+export default Navigation;*/
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navigation = () => {
+return (
+	
+	<>
+	<di className= "nav">
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<NavLink to='/about' activeStyle>
+			About
+		</NavLink>
+		<NavLink to='/events' activeStyle>
+			Events
+		</NavLink>
+		<NavLink to='/annual' activeStyle>
+			Annual Report
+		</NavLink>
+		<NavLink to='/team' activeStyle>
+			Teams
+		</NavLink>
+		<NavLink to='/blogs' activeStyle>
+			Blogs
+		</NavLink>
+		<NavLink to='/sign-up' activeStyle>
+			Sign Up
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+
+	</di>
+	</>
+);
+};
 
 export default Navigation;
