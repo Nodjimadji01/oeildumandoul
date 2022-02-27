8f27cdd64e4d015504ff9c7b1a27d5935ec620ed
root <djeradegolbeparfait@gmail.com>
2022-02-27 06:19:20 +0100

    Footer

--- oeildumandoul/resources/js/components/footer.js
+++ oeildumandoul/resources/js/components/footer.js
@@ -1,8 +1,28 @@
+import React from "react";
 const Footer = () => {
     return (
 
         <div className="footer">
-              <p>Pied du page</p>
+            <nav className="nav_footer">
+                <ul className="ul_footer" >
+
+                    <li className="li_footer" >
+                        <a>A propos nous</a>
+                    </li>
+                    <li className="li_footer" >
+                        <a>Condition d'utilisation</a>
+                    </li>
+                    <li className="li_footer" >
+                        <a>Politique de confidentialité</a>
+                    </li>
+                    <li className="li_footer" >
+                        <a>Contact</a>
+                    </li>
+
+                </ul>
+
+            </nav>
+
         </div>
 
      );
